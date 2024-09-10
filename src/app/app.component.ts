62414c7e225b8f92b3523cd544269469db0cf3f8
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2024-09-10 14:07:14 -0400

    Added primeng and tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Lara } from 'primeng/themes/lara';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'appointments';
+  constructor(private config: PrimeNGConfig) {
+    this.config.theme.set({
+      preset: Lara,
+      options: {
+        darkModeSelector: '.dark-mode',
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities',
+        },
+      },
+    });
+    this.config.ripple.set(true);
+  }
 }
