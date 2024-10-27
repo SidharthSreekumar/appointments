f24899793d8e782c5b2c23cd281e2ae7e82892b7
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2024-10-27 14:12:09 -0400

    Replaced codebase with updated angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimeNGConfig } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
-import { Lara } from 'primeng/themes/lara';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'appointments';
-  constructor(private config: PrimeNGConfig) {
-    this.config.theme.set({
-      preset: Lara,
-      options: {
-        darkModeSelector: '.dark-mode',
-      },
-    });
-    this.config.ripple.set(true);
-  }
 }
